e5333c84cf838233bcfe07c2e18d095329ba38c3
Aloysius Chan <aloysius.chan@news.com.au>
2015-01-30 17:19:56 +1100

    Added sass usage with browsersync

--- recipes/browsersync/gulpfile.js
+++ recipes/browsersync/gulpfile.js
@@ -1,11 +1,29 @@
 var gulp = require('gulp');
 var browsersync = require('browser-sync');
 var reload = browsersync.reload;
+var sass = require('gulp-sass');
 
+// Start a server
 gulp.task('serveit', function () {
     browsersync({
         server: {baseDir: 'serve'}
     });
 });
 
-gulp.watch('**/*', {cwd: 'serve'}, reload);
+// Compile and reload sass
+gulp.task('reloadSass', function () {
+    return gulp.src('**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./'))
+        .pipe(reload({
+            stream: true
+        }));
+});
+
+gulp.task('default', ['serveit'], function () {
+    // Reload updated js and html files
+    gulp.watch(['**/*.js', '**/*.html'], {cwd: 'serve'}, reload);
+    // Reload updated scss files
+    gulp.watch(['**/*.scss'], {cwd: 'serve'}, ['reloadSass']);
+});
+
